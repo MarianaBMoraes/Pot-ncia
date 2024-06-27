b7fcafefdce9f06917bd87f545676ad0590c1ba2
Mariana <marianabmoraes.ef@gmail.com>
2024-06-27 08:43:27 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const prompt = require("prompt-sync")();
+
+let base = Number(prompt("Qual número será elevado: "));
+let expoente = Number(prompt("Digite a potência: "));
+let resultado = 0;
+
+console.log(typeof resultado);
